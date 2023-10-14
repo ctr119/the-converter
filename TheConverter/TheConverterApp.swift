19e54a44666ca197b27c4bea10717c952b3b24af
Victor Barrios <victor.barrios@bestsecret.com>
2023-10-14 23:13:12 +0200

    Initial Commit

---
+++ TheConverter/TheConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheConverterApp.swift
+//  TheConverter
+//
+//  Created by Barrios, Victor on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TheConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
